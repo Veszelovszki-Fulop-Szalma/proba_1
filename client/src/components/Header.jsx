aaeea41a600d97a479affb3c5f31345aa222f8f3
Urvagy <urvagy@gmail.com>
2023-02-08 18:50:11 +0100

    header és a róllunk rész part 1

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 import "./Header.css";
-import Navbar from "./Navbar";
-import kep from "../img/bank.png"
 
 const Header = () => {
   return (
-    <div className="container">
-      
-    </div>
+    <div className="fejlec"/>
   );
 };
 
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -3,15 +3,24 @@ import "./Home.css";
 
 const Home = () => {
   return (
-    <div>
-      <div className="fejlec"/>
-      <h1>Pénzügy</h1> 
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores alias suscipit placeat et eveniet facilis quam iure! Commodi sapiente, cumque, laborum laudantium dolore dolorum placeat, voluptatum aperiam ex ullam maiores.
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium accusamus saepe quasi cumque, reiciendis error? Repellendus, repellat deserunt reprehenderit nemo voluptates animi ea sit illum sequi autem amet voluptate similique!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit tenetur est velit illum numquam repudiandae omnis placeat obcaecati officiis totam accusamus corporis dolor ratione, enim voluptatem magnam! Dolorem, assumenda blanditiis.
-      </p>
+    <div className="container">
+    <h1>Rólunk</h1> 
+    <p>
+                Az AssistedPro az első német vagyoni tanácsadó vállalatok egyike, 1970-ben Kölnben alapították. Azóta törekszünk arra, hogy ügyfeleink számára magas színvonalú pénzügyi tanácsadást nyújtsunk. Az AssistedPro több mint 45 év alatt Európa egyik vezető pénzügyi szolgáltató cégévé vált. Jelenleg 15 országban vagyunk jelen. Magyarország 1992 óta része a vállalatcsoportnak, megbízhatóan, stabilan és kiszámítható módon végezzük munkánkat, amelynek középpontjában a pénzügyileg tervezhető jövő biztosítása, és ez által az európai biztosítási kultúra magyar piacon történő elterjesztése áll. Múltunk a garancia arra, hogy a jövőben is számíthat ránk pénzügyi kérdésekben.
+                <br/>
+                Tanácsadásunk ügyfeleink pénzügyi helyzetének gondos elemzésén alapul – egyénre szabottan. A személyes megbeszélés során főként a célokra vagyunk kíváncsiak, és ezekből alakítunk ki egyéni pénzügyi megoldásokat. Pénzügyi szakembereink általában éveken keresztül kísérik figyelemmel ügyfeleik pénzügyi döntéseit, és azokat mindig az adott élethelyzethez igazítják. Az eredmény mindig az egyén szükségleteinek megfelelő biztosítási, befektetési koncepció.
+                
+    </p>
+    <div className="kep1"/>
+
+      <section class="szurke-hatter">
+            <h2>Motivációnk</h2>
+      </section>
+
+
+
     </div>
+
   );
 };
 
