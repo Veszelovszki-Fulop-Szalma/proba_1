f8081778f6bd0095f3b805a52fae4e2721148e62
Urvagy <urvagy@gmail.com>
2023-02-08 16:14:00 +0100

    kész footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,15 +3,15 @@ import React from "react";
 
 const Footer = () => {
   return (
-
     <footer class="py-4 bg-dark text-white-50">
     <div class="text-center">
-        <small>
-      AssistedPro <br />
-      6722 Szeged <br />
-      Lidecei tér 5. <br />
-      Telefon: +36 55 555-5555 <br />
-       </small>
+      <div>
+        <p>Powered by AssistedPro</p>
+        <p>6722 Szeged</p>
+        <p>Lidecei tér 5.</p>
+        <p>Telefon: +36 55 555-5555</p>
+        <p>All rights reserved © 2023</p>
+      </div>
     </div>
     </footer>
   );
