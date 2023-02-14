2dbd5381b8d28b4dcec77eb3303cc8e2599cb62f
Urvagy <urvagy@gmail.com>
2023-02-14 10:25:11 +0100

    kötvény oldal továbgondolás

--- client/src/pages/Kotveny/Kotveny.jsx
+++ client/src/pages/Kotveny/Kotveny.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import Header2 from "../../components/Header2";
+import "./Kotveny.css";
 
 const Kotveny = () => {
   return <div>
     <Header2/>
-    Kotveny
+    <div className="container">
+    <div className="sorkikot">
+      <h1>Kötvények</h1>
+        <p>
+          A történelem első ismert kötvényét körülbelül Kr.e.2400 körül „bocsátották ki” a mezopotámiai Nippur városában. A megbízó számára garantálta a gabona kifizeté
+          A kötvénypiac mára már a világ legnagyobb piaca, 2020-ban értéke meghaladta a 123 billió dollárt 
+        </p>
+    </div>
 
+    <div className="kepkot"/>
+  
+    <h2>
+      Kötvenyek fajtái
+    </h2>
+
+  </div>
   </div>;
 };
 
