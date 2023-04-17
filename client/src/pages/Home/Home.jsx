9ec3031d8801dec71168ce9c4867aacc3ff5004e
fulike10 <fulop.krisztian2001@gmail.com>
2023-04-17 10:03:17 +0200

    home refresh

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,66 +1,99 @@
 import React from "react";
 import Header from "../../components/Header";
 import { Card } from "react-bootstrap";
+
 import "./Home.css";
 
 import myImage from "../../img/pics1.png";
 import myImage1 from "../../img/pics2.png";
 import myImage2 from "../../img/pics3.png";
 import myImage3 from "../../img/pics4.png";
+import myImage4 from "../../img/handshake.png";
+import myImage5 from "../../img/agreement.png";
+import myImage6 from "../../img/like.png";
+import myImage7 from "../../img/twitter.png";
+import myImage10 from "../../img/twitter2.png";
+import myImage11 from "../../img/twitter3.png";
+
+
+
+
+
 
 const Home = () => {
-  const CardStyle = {
-    position: "static",
-  };
+   const CardStyle = {
+     position: "static",
+     
+   };
+
   return (
-    <div>
+    <div className>
       <Header />
       <div className="container">
-        <div className="szurke-hatter"> <h1>Rólunk</h1> </div>
-        
-        <div className="sorki">
-          <p>
+        <div>
+          {" "}
+          <h2 className="title">
+            Az AssistedPro az ön pénzügyi iránytűje
+          </h2>{" "}
+          <p className="title2">
             Az AssistedPro az első német vagyoni tanácsadó vállalatok egyike,
             1970-ben Kölnben alapították. Azóta törekszünk arra, hogy ügyfeleink
             számára magas színvonalú pénzügyi tanácsadást nyújtsunk. Az
             AssistedPro több mint 45 év alatt Európa egyik vezető pénzügyi
-            szolgáltató cégévé vált. Jelenleg 15 országban vagyunk jelen.
-            Magyarország 1992 óta része a vállalatcsoportnak, megbízhatóan,
-            stabilan és kiszámítható módon végezzük munkánkat, amelynek
-            középpontjában a pénzügyileg tervezhető jövő biztosítása, és ez
-            által az európai biztosítási kultúra magyar piacon történő
-            elterjesztése áll. Múltunk a garancia arra, hogy a jövőben is
-            számíthat ránk pénzügyi kérdésekben.
-            <br />
-            Tanácsadásunk ügyfeleink pénzügyi helyzetének gondos elemzésén
-            alapul – egyénre szabottan. A személyes megbeszélés során főként a
-            célokra vagyunk kíváncsiak, és ezekből alakítunk ki egyéni pénzügyi
-            megoldásokat. Pénzügyi szakembereink általában éveken keresztül
-            kísérik figyelemmel ügyfeleik pénzügyi döntéseit, és azokat mindig
-            az adott élethelyzethez igazítják. Az eredmény mindig az egyén
-            szükségleteinek megfelelő biztosítási, befektetési koncepciója.
-          </p>
-        </div>
-
-        <div className="kep1" />
-
-        <div className="szurke-hatter">
-          <h2>Motivációnk</h2>
-        </div>
-
+            szolgáltató cégévé vált.
+          </p>{" "}
+        </div>{" "}
         <div className="row">
-          <Card className="col-sm-12 col-md-6" style={CardStyle}>
-            <Card.Img src={myImage} />
-          </Card>
-          <Card className="col-sm-12 col-md-6 card" style={CardStyle}>
-            <Card.Img src={myImage1} />
-          </Card>
-          <Card className="col-sm-12 col-md-6 card" style={CardStyle}>
-            <Card.Img src={myImage2} />
-          </Card>
-          <Card className="col-sm-12 col-md-6 card" style={CardStyle}>
-            <Card.Img src={myImage3} />
-          </Card>
+          <div className="col-sm-4">
+            <img src={myImage4} className="kep2" alt="handshake"></img>
+          </div>
+          <div className="col-sm-4 ">
+            <img src={myImage5} className="kep2" alt="donedeal"></img>
+          </div>
+          <div className="col-sm-4 ">
+            <img src={myImage6} className="kep2" alt="like"></img>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-4">
+            <p className="kiemeles">9000 +</p>
+            <p className="tik">Megkötött szerződés</p>
+          </div>
+          <div className="col-sm-4 ">
+            <p className="kiemeles">2500 +</p>
+            <p className="tik">Élő szerződés</p>
+          </div>
+          <div className="col-sm-4 ">
+            <p className="kiemeles">6000 +</p>
+            <p className="tik">Elégedett ügyfél</p>
+          </div>
+        </div>
+        <div className="idezet">
+          <h4 className="lol">Motivációnk</h4>
+
+          <div className="row">
+            <Card className="col-sm-12 col-md-6" style={CardStyle}>
+              <Card.Img src={myImage} />
+            </Card>
+            <Card className="col-sm-12 col-md-6 card" style={CardStyle}>
+              <Card.Img src={myImage1} />
+            </Card>
+            <Card className="col-sm-12 col-md-6 card" style={CardStyle}>
+              <Card.Img src={myImage2} />
+            </Card>
+            <Card className="col-sm-12 col-md-6 card" style={CardStyle}>
+              <Card.Img src={myImage3} />
+            </Card>
+          </div>
+        </div>
+        <h4 className="lol">Ügyfeleink visszajelzései</h4>
+        <div className="scroll">
+          <img src={myImage7} className="kep4" alt="asd"></img>
+          <img src={myImage10} className="kep4" alt="asd"></img>
+          <img src={myImage11} className="kep4" alt="asd"></img>
+        </div>
+        <div className="idezet2">
+          <p>"Jobb a pénzről egy órát beszélni, mint egy hónapot dolgozni érte"</p>
         </div>
       </div>
     </div>
